33d8f66347c52f841482607a74a303d055a8414b
Kiran <kirankm95@gmail.com>
2026-02-19 22:07:00 +0530

    new shell script

--- 06-username-password.sh
+++ 06-username-password.sh
@@ -6,4 +6,4 @@ read $VAR1
 echo "please enter your password:"
 read -s $VAR2
 
-echo "both username and password entered are: $VAR1, $VAR2"
+echo "both username and password entered are: ${VAR1}, ${VAR2}"
