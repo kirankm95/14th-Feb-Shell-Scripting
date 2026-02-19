ff5a501c5293a7c29f497ba0ed617310103d6466
Kiran <kirankm95@gmail.com>
2026-02-19 22:13:15 +0530

    new shell script

--- 08-arrays.sh
+++ 08-arrays.sh
@@ -3,3 +3,9 @@
 VAR1=("Kiran" "Ramaya" "Chinni")
 
 echo "first array value is: ${VAR1[0]}"
+echo "second array value is: ${VAR1[1]}"
+echo "thrid array value is: ${VAR1[2]}"
+echo "all array value is: ${VAR1[@]}"
+echo "count array value is: ${VAR1[#]}"
+echo "* array value is: ${VAR1[*]}"
+echo "last N array value is: ${VAR1[N]}"
