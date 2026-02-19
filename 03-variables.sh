6121d3fc745e0ce0c2607ebc3959a4b43209fbe2
Kiran <kirankm95@gmail.com>
2026-02-19 22:00:07 +0530

    new shell script

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+VAR1=Kiran
+VAR2=Ramya
+
+echo "$VAR1: Hello, how are you"
+echo "$VAR2: Hi, i am good"
+echo "$VAR1: What Chinni is doing"
+echo "$VAR2: Chinni is playing well"
+echo "$VAR1: what is for dinner"
+echo "$VAR2: Chapati and rice and curry"
