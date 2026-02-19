806143e6867733439b32b1577fb76745dc324a5a
Kiran <kirankm95@gmail.com>
2026-02-19 21:58:50 +0530

    new shell script

---
+++ 02-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Kiran: Hello, how are you"
+echo "Ramya: Hi, i am good"
+echo "Kiran: What Chinni is doing"
+echo "Ramya: Chinni is playing well"
+echo "Kiran: what is for dinner"
+echo "Ramya: Chapati and rice and curry"
