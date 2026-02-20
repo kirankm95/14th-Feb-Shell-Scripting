4ed3056655144cc04e8c63ae07e04e9ed9389ffa
Kiran <kirankm95@gmail.com>
2026-02-20 15:31:54 +0530

    new shell script

--- 10-install-package.sh
+++ 10-install-package.sh
@@ -15,7 +15,7 @@ VALIDATE(){
         echo "installation of $2 is success"
     else
         echo "installation of $2 is f...edup failed"
-}
+        }
 
 echo "this $0 started executing at $DATE and will store logs into $LOGFILE"
 
