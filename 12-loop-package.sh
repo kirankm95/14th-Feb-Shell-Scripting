099d5ff5c654e0732cbe6efa55316c641aee2662
Kiran <kirankm95@gmail.com>
2026-02-20 15:45:25 +0530

    new shell script

---
+++ 12-loop-package.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+VAR1=$@
+USER=$(id -u)
+DATE=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$DATE"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$G installation of $2 is success $N"
+    else
+        echo -e "$R installation of $2 is f...edup failed $N"
+    fi
+        }
+
+echo "this $0 started executing at $DATE and will store logs into $LOGFILE" &>> $LOGFILE
+
+if [ $USER -eq 0 ]
+then
+    echo -e "$G You are root user$N, hence we are procedding further"
+else
+    echo -e "$R You are not user$N, so exiting"
+    exit 1
+fi
+
+# yum install ${VAR1} -y &>> $LOGFILE
+# VALIDATE $? ${VAR1}
+
+# yum install ${VAR2} -y &>> $LOGFILE
+# VALIDATE $? ${VAR2}
+
+echo "mentioned packages are $VAR1"
