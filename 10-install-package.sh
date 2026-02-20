eccf9d9622e9ca3fd1c6567d45aff41db14f6014
Kiran <kirankm95@gmail.com>
2026-02-20 15:17:49 +0530

    new shell script

---
+++ 10-install-package.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+VAR1=$1
+USER=$(id -u)
+
+if [ $USER -eq 0 ]
+then
+    echo "You are root user, hence we are procedding further"
+else
+    echo "You are not user, so exiting"
+    exit 1
+fi
+
+yum install $VAR1
