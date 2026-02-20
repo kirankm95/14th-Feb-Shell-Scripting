db53580a4f0b600fb3689bd7fac93d777690aea0
Kiran <kirankm95@gmail.com>
2026-02-20 15:27:41 +0530

    new shell script

--- 10-install-package.sh
+++ 10-install-package.sh
@@ -4,12 +4,16 @@ VAR1=$1
 USER=$(id -u)
 DATE=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$DATE"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 echo "this $0 started executing at $DATE and will store logs into $LOGFILE"
 
 if [ $USER -eq 0 ]
 then
-    echo "You are root user, hence we are procedding further"
+    echo -e "$G You are root user$N, hence we are procedding further"
 else
     echo "You are not user, so exiting"
     exit 1
